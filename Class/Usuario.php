466f0628248a6510e139ae016ebf1e7e60d74d14
Giovani G Chaves <giovanigchaves.engenharia@gmail.com>
2018-04-19 18:51:11 -0300

    novos metodos inplementados

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -13,40 +13,68 @@ class Usuario {
     /*
      * Obtem id_usuario
      */
+    /**
+     * @return mixed
+     */
     public function getIdusuario() {
         return $this->idusuario;
     }
 
+    /**
+     * @param $value
+     */
     public function setIdusuario($value) {
         $this->idusuario = $value;
     }
 
+    /**
+     * @return mixed
+     */
     public function getDeslogin() {
         return $this->deslogin;
     }
 
+    /**
+     * @param $value
+     */
     public function setDeslogin($value) {
+
         $this->deslogin = $value;
     }
 
+    /**
+     * @return mixed
+     */
     public function getDessenha() {
         return $this->dessenha;
     }
 
+    /**
+     * @param $value
+     */
     public function setDessenha($value) {
         $this->dessenha = $value;
     }
 
+    /**
+     * @return mixed
+     */
     public function getDtcadastro() {
         return $this->dtcadastro;
     }
 
+    /**
+     * @param $value
+     */
     public function setDtcadastro($value) {
         $this->dtcadastro = $value;
     }
 
+    /**
+     * @param $id
+     */
     public function loadById($id){
-        
+
         $sql = new Sql();
         
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE id_usuario = :ID",array(":ID"=>$id));
@@ -62,7 +90,59 @@ class Usuario {
             
         }
     }
-    
+
+    /**
+     * @return array
+     */
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+
+    }
+
+    /**
+     * @param $login
+     *
+     * @return array
+     */
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin",array(':SEARCH'=>"%".$login."%"));
+    }
+
+    public function login($login, $password){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if(count($results) > 0){
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['id_usuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        }else{
+            throw new Exception("Login e/ou senha invalidos.");
+        }
+
+    }
+
+
+    /**
+     * @return string
+     */
     public function __toString(){
         
         return json_encode(array(
--- index.php
+++ index.php
@@ -8,18 +8,23 @@
 
     require_once("config.php");
 
-$root = new Usuario();
+    //Carrega um usuario
+    //$root = new Usuario();
+    //$root->loadById(3);
+    //echo $root;
 
-$root->loadById(3);
+    // Carrega uma lista de usuarios
+    //    $lista = Usuario::getList();
+    //    echo json_encode($lista);
 
-echo $root;
+    //carrega uma lista de usuaruis buscado pelo login.
+    //$search = Usuario::search("us");
+    //echo json_encode($search);
 
-
-
-
-
-
-
+    // carrega um usuario usando o login e senha
+    //$usuario = new Usuario();
+    //$usuario->login("root", "$#@*&");
+    //echo $usuario;
 
 
 
